6b536d5e19c9d933280ea8606a9bd1045c288818
mugilanmonish <mugilanmonish@gmail.com>
2025-01-13 14:48:11 +0530

    Updated webCapability file - firefox maxInstance changed to 1

--- genricUtility/webCapabilities.js
+++ genricUtility/webCapabilities.js
@@ -19,7 +19,7 @@ const chromeLocal = {
 }
 
 const firefoxLocal = {
-    maxInstances: 2,
+    maxInstances: 1,
     browserName: 'firefox',
     browserVersion: process.env.BROWSER_VERSION !== null ? process.env.BROWSER_VERSION : 'latest',
     'moz:firefoxOptions': process.env.DOCKER === 'true' ? {
